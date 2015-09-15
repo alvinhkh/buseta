e1093ea64e589d53cb9562f23e549143973fac08
AlvinHKH <email@alvinhkh.com>
2015-09-15 18:08:30 +0800

    add ACRA

---
+++ app/src/main/java/com/alvinhkh/buseta/Acra.java
@@ -0,0 +1,26 @@
+package com.alvinhkh.buseta;
+
+import android.app.Application;
+
+import org.acra.*;
+import org.acra.annotation.*;
+
+@ReportsCrashes(
+        formUri = "https://alvinhkh.cloudant.com/acra-buseta/_design/acra-storage/_update/report",
+        reportType = org.acra.sender.HttpSender.Type.JSON,
+        httpMethod = org.acra.sender.HttpSender.Method.PUT,
+        formUriBasicAuthLogin="whingerriandesionfeeksou",
+        formUriBasicAuthPassword="3316b6e0c5b2371ec8cff1652557378d649abec3",
+        // usual configuration
+        mode = ReportingInteractionMode.SILENT
+)
+
+public class Acra extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // The following line triggers the initialization of ACRA
+        ACRA.init(this);
+    }
+}
