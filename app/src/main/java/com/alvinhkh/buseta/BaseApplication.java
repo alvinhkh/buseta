3f1bcd6e7f7385bfe55931bb3c4d5350a58d9da1
AlvinHKH <email@alvinhkh.com>
2017-06-11 12:30:17 +0800

    acra to firebase crash

--- app/src/main/java/com/alvinhkh/buseta/BaseApplication.java
+++ app/src/main/java/com/alvinhkh/buseta/BaseApplication.java
@@ -3,31 +3,19 @@ package com.alvinhkh.buseta;
 import android.app.Application;
 
 import com.alvinhkh.buseta.utils.NightModeHelper;
-
-import org.acra.*;
-import org.acra.annotation.*;
-
-@ReportsCrashes(
-        formUri = "https://alvinhkh.cloudant.com/acra-buseta/_design/acra-storage/_update/report",
-        reportType = org.acra.sender.HttpSender.Type.JSON,
-        httpMethod = org.acra.sender.HttpSender.Method.PUT,
-        formUriBasicAuthLogin="whingerriandesionfeeksou",
-        formUriBasicAuthPassword="3316b6e0c5b2371ec8cff1652557378d649abec3",
-        // usual configuration
-        mode = ReportingInteractionMode.SILENT
-)
+import com.google.firebase.crash.FirebaseCrash;
 
 public class BaseApplication extends Application {
 
     @Override
     public void onCreate() {
-
         super.onCreate();
-
-        if (!BuildConfig.DEBUG) ACRA.init(this); // filter out debugging
-
+        if (BuildConfig.DEBUG) {
+            FirebaseCrash.setCrashCollectionEnabled(false);
+        } else {
+            FirebaseCrash.setCrashCollectionEnabled(true);
+        }
         NightModeHelper.update(this);
-
     }
 
 }
