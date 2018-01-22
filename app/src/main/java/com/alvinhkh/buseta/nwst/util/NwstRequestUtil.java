fee99aa6fc64e23a6cffca54e341e8e09a0104c6
AlvinHKH <email@alvinhkh.com>
2018-01-23 00:44:23 +0800

    move md5 hash function to a class

--- app/src/main/java/com/alvinhkh/buseta/nwst/util/NwstRequestUtil.java
+++ app/src/main/java/com/alvinhkh/buseta/nwst/util/NwstRequestUtil.java
@@ -1,16 +1,12 @@
 package com.alvinhkh.buseta.nwst.util;
 
-import android.support.annotation.NonNull;
 import android.text.TextUtils;
 
 import com.alvinhkh.buseta.model.BusRoute;
 import com.alvinhkh.buseta.nwst.model.NwstVariant;
+import com.alvinhkh.buseta.utils.HashUtil;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.util.Random;
-
-import timber.log.Timber;
 
 public class NwstRequestUtil {
 
@@ -22,7 +18,7 @@ public class NwstRequestUtil {
         String timestamp = Integer.toString(Math.round((float) (System.currentTimeMillis() / 1000)));
         timestamp = timestamp.substring(timestamp.length() - 6);
         String secret = "firstbusmwymwy";
-        return timestamp + randomInt + md5(timestamp + randomInt + secret);
+        return timestamp + randomInt + HashUtil.md5(timestamp + randomInt + secret);
     }
 
     public static String paramInfo(BusRoute busRoute) {
@@ -39,25 +35,4 @@ public class NwstRequestUtil {
         }
         return "1|*|" + busRoute.getCompanyCode() + "||" + variant.getRdv() + "||" + variant.getStartSequence() + "||" + variant.getEndSequence() + "||" + busRoute.getSequence();
     }
-
-    private static String md5(@NonNull String text) {
-        try {
-            MessageDigest digest = MessageDigest.getInstance("MD5");
-            digest.update(text.getBytes());
-            byte messageDigest[] = digest.digest();
-            StringBuilder hexString = new StringBuilder();
-            for (byte aMessageDigest : messageDigest) {
-                StringBuilder h = new StringBuilder(Integer.toHexString(0xFF & aMessageDigest));
-                while (h.length() < 2) {
-                    h.insert(0, "0");
-                }
-                hexString.append(h);
-            }
-            return hexString.toString();
-        } catch (NoSuchAlgorithmException e) {
-            Timber.d(e);
-        }
-        return null;
-    }
-
 }
---
+++ app/src/main/java/com/alvinhkh/buseta/utils/HashUtil.java
@@ -0,0 +1,45 @@
+package com.alvinhkh.buseta.utils;
+
+import android.support.annotation.NonNull;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import timber.log.Timber;
+
+public class HashUtil {
+
+    public static String md5(@NonNull String text) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("MD5");
+            digest.update(text.getBytes());
+            return hexString(digest.digest());
+        } catch (NoSuchAlgorithmException e) {
+            Timber.d(e);
+        }
+        return null;
+    }
+
+    public static String sha1(@NonNull String text) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-1");
+            digest.update(text.getBytes());
+            return hexString(digest.digest());
+        } catch (NoSuchAlgorithmException e) {
+            Timber.d(e);
+        }
+        return null;
+    }
+
+    public static String hexString(byte[] bytes) {
+        StringBuilder hexString = new StringBuilder();
+        for (byte aMessageDigest : bytes) {
+            StringBuilder h = new StringBuilder(Integer.toHexString(0xFF & aMessageDigest));
+            while (h.length() < 2) {
+                h.insert(0, "0");
+            }
+            hexString.append(h);
+        }
+        return hexString.toString();
+    }
+}
