adaba6bec735ae6aff836f5c46600d11daca41d0
AlvinHKH <email@alvinhkh.com>
2018-08-09 22:57:14 +0800

    fix missing suggestion database migration

--- app/src/main/java/com/alvinhkh/buseta/search/dao/SuggestionDatabase.kt
+++ app/src/main/java/com/alvinhkh/buseta/search/dao/SuggestionDatabase.kt
@@ -17,31 +17,47 @@ abstract class SuggestionDatabase : RoomDatabase() {
     companion object {
         private var instance: SuggestionDatabase? = null
 
+        private fun MIGRATION_1_to_5(db: SupportSQLiteDatabase) {
+            db.execSQL(("CREATE TABLE " + Suggestion.TABLE_NAME + "_TEMP ("
+                    + Suggestion.COLUMN_ID + " INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, "
+                    + Suggestion.COLUMN_TEXT + " TEXT NOT NULL, "
+                    + Suggestion.COLUMN_TYPE + " TEXT NOT NULL, "
+                    + Suggestion.COLUMN_TIMESTAMP + " INTEGER NOT NULL, "
+                    + Suggestion.COLUMN_COMPANY + " TEXT NOT NULL DEFAULT '', "
+                    + "UNIQUE (" + Suggestion.COLUMN_TEXT + ", " + Suggestion.COLUMN_COMPANY
+                    + ", " + Suggestion.COLUMN_TYPE + ") ON CONFLICT REPLACE"
+                    + ");"))
+            db.execSQL("CREATE UNIQUE INDEX index_" + Suggestion.TABLE_NAME + "_"
+                    + Suggestion.COLUMN_COMPANY + "_" + Suggestion.COLUMN_TEXT + "_"
+                    + Suggestion.COLUMN_TYPE + " ON " + Suggestion.TABLE_NAME + "_TEMP ("
+                    + Suggestion.COLUMN_COMPANY + ", " + Suggestion.COLUMN_TEXT + ", "
+                    + Suggestion.COLUMN_TYPE + ");")
+            db.execSQL("INSERT INTO " + Suggestion.TABLE_NAME + "_TEMP (" +
+                    Suggestion.COLUMN_TEXT + ", " + Suggestion.COLUMN_TYPE + ", " + Suggestion.COLUMN_TIMESTAMP +
+                    ") SELECT " +
+                    Suggestion.COLUMN_TEXT + ", " + Suggestion.COLUMN_TYPE + ", " + Suggestion.COLUMN_TIMESTAMP +
+                    " FROM " + Suggestion.TABLE_NAME)
+            db.execSQL("UPDATE " + Suggestion.TABLE_NAME + "_TEMP SET " + Suggestion.COLUMN_COMPANY + " = 'KMB' WHERE (" +
+                    Suggestion.COLUMN_COMPANY + " IS NULL OR " + Suggestion.COLUMN_COMPANY + " IS ''" + ")")
+            db.execSQL("DROP TABLE IF EXISTS " + Suggestion.TABLE_NAME)
+            db.execSQL("ALTER TABLE " + Suggestion.TABLE_NAME + "_TEMP RENAME TO " + Suggestion.TABLE_NAME)
+        }
+
         private var MIGRATION_1_5 = object: Migration(1, 5) {
             override fun migrate(db: SupportSQLiteDatabase) {
-                db.execSQL(("CREATE TABLE " + Suggestion.TABLE_NAME + "_TEMP ("
-                        + Suggestion.COLUMN_ID + " INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, "
-                        + Suggestion.COLUMN_TEXT + " TEXT NOT NULL, "
-                        + Suggestion.COLUMN_TYPE + " TEXT NOT NULL, "
-                        + Suggestion.COLUMN_TIMESTAMP + " INTEGER NOT NULL, "
-                        + Suggestion.COLUMN_COMPANY + " TEXT NOT NULL DEFAULT '', "
-                        + "UNIQUE (" + Suggestion.COLUMN_TEXT + ", " + Suggestion.COLUMN_COMPANY
-                        + ", " + Suggestion.COLUMN_TYPE + ") ON CONFLICT REPLACE"
-                        + ");"))
-                db.execSQL("CREATE UNIQUE INDEX index_" + Suggestion.TABLE_NAME + "_"
-                        + Suggestion.COLUMN_COMPANY + "_" + Suggestion.COLUMN_TEXT + "_"
-                        + Suggestion.COLUMN_TYPE + " ON " + Suggestion.TABLE_NAME + "_TEMP ("
-                        + Suggestion.COLUMN_COMPANY + ", " + Suggestion.COLUMN_TEXT + ", "
-                        + Suggestion.COLUMN_TYPE + ");")
-                db.execSQL("INSERT INTO " + Suggestion.TABLE_NAME + "_TEMP (" +
-                        Suggestion.COLUMN_TEXT + ", " + Suggestion.COLUMN_TYPE + ", " + Suggestion.COLUMN_TIMESTAMP +
-                        ") SELECT " +
-                        Suggestion.COLUMN_TEXT + ", " + Suggestion.COLUMN_TYPE + ", " + Suggestion.COLUMN_TIMESTAMP +
-                        " FROM " + Suggestion.TABLE_NAME)
-                db.execSQL("UPDATE " + Suggestion.TABLE_NAME + "_TEMP SET " + Suggestion.COLUMN_COMPANY + " = 'KMB' WHERE (" +
-                        Suggestion.COLUMN_COMPANY + " IS NULL OR " + Suggestion.COLUMN_COMPANY + " IS ''" + ")")
-                db.execSQL("DROP TABLE IF EXISTS " + Suggestion.TABLE_NAME)
-                db.execSQL("ALTER TABLE " + Suggestion.TABLE_NAME + "_TEMP RENAME TO " + Suggestion.TABLE_NAME)
+                MIGRATION_1_to_5(db)
+            }
+        }
+
+        private var MIGRATION_2_5 = object: Migration(2, 5) {
+            override fun migrate(db: SupportSQLiteDatabase) {
+                MIGRATION_1_to_5(db)
+            }
+        }
+
+        private var MIGRATION_3_5 = object: Migration(3, 5) {
+            override fun migrate(db: SupportSQLiteDatabase) {
+                MIGRATION_1_to_5(db)
             }
         }
 
@@ -76,7 +92,7 @@ abstract class SuggestionDatabase : RoomDatabase() {
                 synchronized(SuggestionDatabase::class.java) {
                     instance = Room.databaseBuilder(context.applicationContext,
                             SuggestionDatabase::class.java, "suggestions.db")
-                            .addMigrations(MIGRATION_1_5, MIGRATION_4_5)
+                            .addMigrations(MIGRATION_1_5, MIGRATION_2_5, MIGRATION_3_5, MIGRATION_4_5)
                             .allowMainThreadQueries()
                             .build()
                 }
